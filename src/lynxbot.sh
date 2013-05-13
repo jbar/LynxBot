57c26493e55db0c7d982c9a1f1ea3e3efaff7555
jbar <jeanjacquesbrucker@gmail.com>
2013-05-13 16:31:31 +0200

    Update (unused) version in configure.ac, autoreconf, fix spaces/tab

--- src/lynxbot.sh
+++ src/lynxbot.sh
@@ -3,11 +3,11 @@
 LynxBotVersion="LynxBot/0.2"
 
 function exitlynxbot {
-    kill $lynxpid
-    kill $teepid
-    rm /tmp/lynx_cmd_input.$$
-    rm /tmp/lynx_output.$$
-    wait
+	kill $lynxpid
+	kill $teepid
+	rm /tmp/lynx_cmd_input.$$
+	rm /tmp/lynx_output.$$
+	wait
 }
 
 trap "exitlynxbot" 0
@@ -17,7 +17,7 @@ mkfifo /tmp/lynx_cmd_input.$$ || exit -1
 mkfifo /tmp/lynx_output.$$ || exit -1
 
 # Set the UserAgent
-   UserAgent="$(lynx -version | head -1)  $LynxBotVersion"
+	UserAgent="$(lynx -version | head -1)  $LynxBotVersion"
 	#UserAgent="Mozilla/5.0 (X11; Linux i686; rv:17.0) Gecko/20100101 Firefox/17.0"
 
 # be sure that lynx will use default english langage
